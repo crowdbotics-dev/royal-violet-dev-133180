cdc98697c18ef5955ff1e1452d8112adc22f520d
Crowdbotics <engineering@crowdbotics.com>
2024-02-09 19:03:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-violet-dev-133180",
+  url: "https://royal-violet-dev-133180.botics.co",
+  api: "https://royal-violet-dev-133180.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
